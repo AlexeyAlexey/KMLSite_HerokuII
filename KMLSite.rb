91a52aea948810e3cdd582afe3e98fbab27db3ce
alexey@alexey-SR58P <alexey.kondratenko@mail.ru>
2013-03-16 04:37:04 +0200

    add files and modifiy

--- KMLSite.rb
+++ KMLSite.rb
@@ -1,8 +1,11 @@
 require "sinatra"
+require 'json'
+
 
 #:app_file - main application file
 set :app_file, __FILE__
 
+Dir.chdir(File.dirname File.expand_path('../KMLSite.rb', __FILE__))
 
 get '/' do
 
@@ -10,10 +13,43 @@ get '/' do
 
 end
 
+get '/views/googlmaps' do
+
+erb :googlmaps, :layout => false
+
+end
+
+get '/kml' do
+
+ send_file "load/kml", :filename => "kml", :type => 'application/vnd.google-earth.kml+xml'
+ 
+
+end
+
+
+
+
 post '/input' do
 
+ begin
+   jsonDate = JSON.parse params[:fixes]
+ rescue => ex # ссылается на обрабатываемый объект Exception
+   return "#{ex.class}: #{ex.message}"
+ end
 
-erb :result, :layout => true, :locals => { :fixes => params[:fixes] }
+#erb :result, :layout => true, :locals => { :fixes => params[:fixes] }
+ view_url = 'http://jgps.me/l/tokentokentoken'
+
+ message = Proc.new do |time, view_url, error| 
+      
+   JSON.generate [{"fix_tlist" => time}, {"meta" => {"view_url" => view_url}}, error]
+ 
+ end
+
+if jsonDate["error"]
+  then return message.call(jsonDate["data"]["t"], view_url, {"error" => jsonDate["error"]})
+  else return message.call(jsonDate["data"]["t"], view_url)
+end 
 
 
 #Get json with coordinate
@@ -28,17 +64,15 @@ end
 post '/load' do
 
 
-fileDir = File.expand_path('../KMLSite.rb', __FILE__)
-fileDir = File.dirname(fileDir) + "/load"
+  #fileDir = File.expand_path('../KMLSite.rb', __FILE__)
+  #fileDir = File.dirname(fileDir) + "/load/"
 
-string_io = request.body # will return a StringIO
-      
-data_bytes = string_io.read # read the stream as bytes
-      
-# create the file path
-path = File.join(fileDir, params[:loadfile])
-# Write it to disk...
-File.open(path, 'w') {|f| f.write(string_io.read) }
+  #File.open(fileDir + params['loadfile'][:filename], "w") do |f|
+    #f.write(params['loadfile'][:tempfile].read)
+  #end
+ 
+#erb :kml, :layout => true, :locals => { :loadfile => params['fixes'][:tempfile] } 
+ return "The file was successfully uploaded! #{params[:fixes]}"
  
 
 end
