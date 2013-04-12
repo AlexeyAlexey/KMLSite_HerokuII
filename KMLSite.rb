5984126e1e800fa0bc7d066c7c3a0fa53d227448
alexey@alexey-SR58P <alexey.kondratenko@mail.ru>
2013-04-12 15:24:40 +0300

    cash_control

--- KMLSite.rb
+++ KMLSite.rb
@@ -67,7 +67,7 @@ constCountR = 10
         headers 'Content-Type' => "application/vnd.google-earth.kml+xml;charset=utf-8" 
         #response.headers['Cache-Control'] = 'no-cash'
         
-    cache_control :public, :must_revalidate, :max_age => 0, :max_stale => 0
+    cache_control :public, :must_revalidate#, :max_age => 0, :max_stale => 0
     erb :kml_kml, :layout => false, :locals => {:gpsData => @gpsData, :markEndPoint => @markEndPoint}
     
 end
