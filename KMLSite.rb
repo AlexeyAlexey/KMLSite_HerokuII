7f9b53433da4d31416642ef48c38a519aca60e3e
alexey@alexey-SR58P <alexey.kondratenko@mail.ru>
2013-04-25 10:27:00 +0300

    delete kml.erb file in root

--- KMLSite.rb
+++ KMLSite.rb
@@ -19,7 +19,7 @@ not_found do
   'This is nowhere to be found.'
 end
 
-ActiveRecord::Base.establish_connection "mysql2://b2de1d2c131a92:ed316f52@us-cdbr-east-03.cleardb.com/heroku_bd8015972428027"  
+ActiveRecord::Base.establish_connection "mysql2://"  
   
 class GpsDate < ActiveRecord::Base
 end
